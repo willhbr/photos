4e93ef243d8ffa81b9f59448b6bc4a39f93ad11b
Will Richardson <will@willhbr.net>
2026-09-22 21:26:28 +1000

    Fixes and stuff

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,9 +1,19 @@
 module CustomFilters
+  def smart_date(input)
+    build_time = @context.registers[:site].time
+    if input.strftime('%F') == build_time.strftime('%F')
+      build_time
+    else
+      input
+    end
+  end
+
   @@randomised = nil
-  def random_post_link(post, all_posts)
+  def random_post_link(post)
+    post ||= @context.registers[:site].posts.docs.sample
     if @@randomised.nil?
       by_url = Hash.new
-      all = all_posts.shuffle
+      all = @context.registers[:site].posts.docs.shuffle
       prev = all[-1]
       all.each do |post|
         by_url[post.url] = prev.url
