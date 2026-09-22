aeffd0d6bd08aa5aac79463fe348a1031ac404f8
Will Richardson <will@willhbr.net>
2026-09-22 21:20:58 +1000

    Fixup randomisation

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,16 +1,17 @@
 module CustomFilters
+  @@randomised = nil
   def random_post_link(post, all_posts)
-    if @randomised.nil?
-      shuf = all_posts.shuffle
+    if @@randomised.nil?
       by_url = Hash.new
       all = all_posts.shuffle
       prev = all[-1]
       all.each do |post|
         by_url[post.url] = prev.url
+        prev = post
       end
-      @randomised = by_url
+      @@randomised = by_url
     end
-    @randomised[post.url]
+    @@randomised[post.url]
   end
 end
 
